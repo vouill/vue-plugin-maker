d8091764ff3688c495440b6f4d366c73e6e25fc9
Vouill <vouillon.thibaud@gmail.com>
2017-02-27 01:39:15 +0100

    Trust me, it's not badly written. It's just way above your head.

--- build/webpack.lib.conf.js
+++ build/webpack.lib.conf.js
@@ -1,10 +1,10 @@
 var webpack = require('webpack');
 var path = require('path');
-var outputFile = libraryName + '.js';
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 
 var libraryName = 'plugin';
+var outputFile = libraryName + '.js';
 
 var config = merge(baseWebpackConfig,{
   entry: path.resolve(__dirname, '../src/plugin/plugin.js'),
